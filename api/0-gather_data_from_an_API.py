e78ddecc20da7b781ae91a2c2fe4712f91362a46
juanRCoder <guillecarck@gmail.com>
2023-09-18 11:26:33 -0700

    Feat: Add a new variable to display the results

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,29 +5,32 @@ from sys import argv
 
 url = 'https://jsonplaceholder.typicode.com'
 
-#Acceso a la API
+# Acceso a la API
 response = requests.get(url)
 
-def make_request():
 
-    #Hacer las solicitudes para obtener los datos de la API
+def make_request():
+    # Hacer las solicitudes para obtener los datos de la API
     response_tasks = requests.get(f"{url}/todos?userId={argv[1]}")
     response_user = requests.get(f"{url}/users/{argv[1]}")
 
+    # Convertir de JSON a estrucutura de datos
+    all_task = response_tasks.json()
+    username = response_user.json().get('name')
 
-    #Convertir de JSON a estrucutura de datos
-    ALL_OF_TASKS = response_tasks.json()
-    EMPLOYEE_NAME = response_user.json().get('name')
-
-    #Lista de tareas completadas por el usuario x
-    NUMBER_OF_DONE_TASKS = []
-    for task in ALL_OF_TASKS:
+    # Lista de tareas completadas por el usuario x
+    completed_tasks = []
+    for task in all_task:
         if task.get('completed'):
-            NUMBER_OF_DONE_TASKS.append(task)
+            completed_tasks.append(task)
 
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks({len(NUMBER_OF_DONE_TASKS)}/{len(ALL_OF_TASKS)}):")
-    for task in NUMBER_OF_DONE_TASKS:
+    text = "is done with tasks"
+    completed = len(completed_tasks)
+    print(f"Employee {username} {text} ({completed}/{len(all_task)}):")
+
+    for task in completed_tasks:
         print(f"\t {task.get('title')}")
+
 
 if __name__ == '__main__':
     make_request()
